5225353c6a7fe51b211973089f8c660b615adb5a
Zachariah Reitano <zreitano@gmail.com>
2013-06-19 16:45:42 -0400

    Group Exercised Improved

--- class_group.rb
+++ class_group.rb
@@ -1,11 +1,10 @@
 puts "How many members would you like in each group?"
 groups_of = gets.chomp.to_i
 
-class_members = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "Zachariah Reitano"]
+class_members = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Zachariah Reitano"]
 groups = []
 
-randoms = class_members.sample(groups_of)
-groups.push randoms
+groups.push class_members.sample(groups_of)
 
 while class_members.length >= groups_of
   randoms = class_members.sample(groups_of)
@@ -18,4 +17,4 @@ unless class_members.empty?
 end
 
 puts""
-groups.each {|group| puts group.flatten.to_s}
+groups.each {|group| puts "Group " + ((groups.index(group) +1)).to_s + ": " +  "\n" + group.flatten.to_s + "\n"}
