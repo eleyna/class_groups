64ab089529d05f3449c9811f1a57dd95897accaf
eleyna <eleyna@gmail.com>
2013-06-19 22:38:01 -0400

    improvements

--- class_group.rb
+++ class_group.rb
@@ -1,20 +1,26 @@
-puts "How many members would you like in each group?"
+puts "\nHow many members would you like in each group?"
 groups_of = gets.chomp.to_i
 
-class_members = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Zachariah Reitano"]
-groups = []
+class_members = ["Alan Appelstein", "Alice Mottola", "Alina Guzman",
+                 "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli",
+                 "Beryl Schragger", "Chris Clearfield", "Christine Coulter",
+                 "Christopher Huyett", "Christopher Wright", "Corbin Sykes",
+                 "Eleyna Whittingham", "Eric Gong", "Jamal Powell",
+                 "Jason Darcy", "Kevon Cheung", "Mike Mayer",
+                 "Mike Vanger", "Nick Lombardi", "Teddy Cleveland",
+                 "Thomas Yu", "Tim Blonski", "Tom Brennan",
+                 "Tricia Dougals", "Zachariah Reitano"]
 
-groups.push class_members.sample(groups_of)
+num_groups = class_members.length / groups_of
 
-while class_members.length >= groups_of
-  randoms = class_members.sample(groups_of)
-  randoms.each {|name| class_members.delete_at(class_members.index(name))}
-  groups.push randoms
+class_members = class_members.shuffle!
+
+for num in (1..num_groups)
+  group = class_members.pop(groups_of)
+  puts "\nGroup " + num.to_s + ":"
+  group.each {|name| puts name.to_s}
+  if num == num_groups
+    class_members.each {|name| puts name.to_s}
+  end
 end
 
-unless class_members.empty?
-  groups[-1].push class_members
-end
-
-puts""
-groups.each {|group| puts "Group " + ((groups.index(group) +1)).to_s + ": " +  "\n" + group.flatten.to_s + "\n"}
